358ad73b73f3bde907822668e786b855a6f51149
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- device.mk
+++ device.mk
@@ -145,13 +145,8 @@ PRODUCT_PACKAGES += \
     
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2.vendor \
     android.hardware.drm-service.clearkey
-    
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor
     
 # Fastbootd
 PRODUCT_PACKAGES += \
