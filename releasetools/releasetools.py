6aa717ca294cf6c576376acfc5b714f11bec3019
Tim Zimmermann <tim@linux4.de>
2022-07-06 13:10:29 +0200

    venus:[dynamic_partitions]: Flash super_empty on initial flash
    Change-Id: I3a56ec83aeb633ef751f287d6ab739e374ce6a28

---
+++ releasetools/releasetools.py
@@ -0,0 +1,37 @@
+#!/bin/env python3
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import common
+import re
+
+def AddImage(info, dir, basename, dest, printInfo=True):
+  data = info.input_zip.read(dir + "/" + basename)
+  common.ZipWriteStr(info.output_zip, basename, data)
+  if printInfo:
+    info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
+def ImageExists(info, dir, basename):
+  try:
+    info.input_zip.getinfo(dir + "/" + basename)
+    return True
+  except KeyError:
+    return False
+
+def FullOTA_InstallBegin(info):
+  if ImageExists(info, "IMAGES", "super_empty.img"):
+    AddImage(info, "IMAGES", "super_empty.img", "/tmp/super_empty.img", False)
+    info.script.AppendExtra('huawei.retrofit_dynamic_partitions();')
