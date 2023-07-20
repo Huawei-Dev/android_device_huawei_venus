bd1702ce1df7a30a0bd1fd4ef9385b191307b684
R0rt1z2 <roger@r0rt1z2.com>
2023-07-20 17:31:14 +0000

    Build hisi livedisplay sysfs service
    Change-Id: Idb8b81a7e1cf89902fa8287ccdc14813b3528b50

--- device.mk
+++ device.mk
@@ -191,6 +191,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.hi6250
     
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service-hisi
+
 # MAC Loader
 PRODUCT_PACKAGES += \
     mac_nvme
