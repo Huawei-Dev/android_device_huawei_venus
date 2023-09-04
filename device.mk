906241f34858ae381d6a8d5dd5c92fb48945860c
Chenyang Zhong <zhongcy95@gmail.com>
2023-09-04 20:51:50 +0000

    ship cgroups.json and task_profiles.json in vendor
    Google's prebuilt cgroups.json and task_profiles.json for
    products launched with previous API levels only covers
    ro.product.first_api_level >= 28. [1]
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I33c0540ef50054fad839886f9f014cc275659e77

--- device.mk
+++ device.mk
@@ -253,6 +253,10 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.hisi-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
     
 # Pcap
 PRODUCT_PACKAGES += \
