a9d679854a59621b13b11a6e23c8e79ab5546575
Huawei-Dev <surdu_petru@yahoo.com>
2023-11-10 21:20:19 +0100

    clean unused permissions

--- device.mk
+++ device.mk
@@ -329,11 +329,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     sys.usb.configfs=1 \
     sys.usb.controller=hisi-usb-otg
     
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/privapp-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions.xml \
-    $(LOCAL_PATH)/prebuilts/privapp-permissions-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-system.xml \
-    $(LOCAL_PATH)/prebuilts/whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/whitelist.xml
-    
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
