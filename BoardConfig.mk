94d0a80dcfcfa0e14e79f7f9af087b075ba0b40d
Huawei-Dev <surdu_petru@yahoo.com>
2023-11-11 23:01:09 +0100

    remove duplicate TARGET_VENDOR_PROP into BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,9 +122,6 @@ BOARD_ROOT_EXTRA_SYMLINKS += /vendor/odm/hw_odm:/hw_odm
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# Props
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.hi6250
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
