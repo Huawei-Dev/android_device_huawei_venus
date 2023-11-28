e5f5293a4513d543ed0761dee741f9e2bd4ac1fc
Huawei-Dev <surdu_petru@yahoo.com>
2023-11-28 18:17:29 +0100

    build audio.primary.hisi_wrapper & android.hardware.audio.service.hisi

--- device.mk
+++ device.mk
@@ -120,6 +120,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Audio
 PRODUCT_PACKAGES += \
+    audio.primary.hisi_wrapper \
     audio.primary.default \
     audio.a2dp.default \
     audio.usb.default \
@@ -133,7 +134,7 @@ PRODUCT_PACKAGES += \
     tinypcminfo
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio.service \
+    android.hardware.audio.service.hisi \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@2.0-impl \
