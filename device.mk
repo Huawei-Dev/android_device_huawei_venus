e682639659567a268795afd0fb057fd2a3f0d1a2
Huawei-Dev <surdu_petru@yahoo.com>
2023-11-28 14:46:13 +0100

    venus: build missing drm blobs

--- device.mk
+++ device.mk
@@ -149,6 +149,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
     
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor
+    
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
