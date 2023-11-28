807db3593d87e849e2e34b914c7e1d61d670d149
Huawei-Dev <surdu_petru@yahoo.com>
2023-11-28 15:16:30 +0100

    build missing GPS blobs

--- device.mk
+++ device.mk
@@ -71,6 +71,9 @@ PRODUCT_COPY_FILES += \
     
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
+    
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor
 
 # Permissions
 PRODUCT_COPY_FILES += \
