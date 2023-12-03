9bac021c8e2cfb7fb868bb262ade13938ed05c1d
Huawei-Dev <surdu_petru@yahoo.com>
2023-12-03 08:21:36 +0100

    venus: languages_full.mk it's already included

--- lineage_venus.mk
+++ lineage_venus.mk
@@ -14,7 +14,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 
 # Inherit some common CM stuff.
