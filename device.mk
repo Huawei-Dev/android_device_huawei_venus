Huawei-Dev/android_device_huawei_hi3650

also install fstab.hi3650 to ramdisk

device.mk
@@ -239,6 +239,7 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.hi3650 \
+    fstab.hi3650.ramdisk \
     fstab.modem \
     init.charger.rc \
     init.hi3650.connectivity.rc \

Huawei-Dev/android_device_huawei_venus

device.mk
@@ -276,6 +276,7 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.hi6250 \
+    fstab.hi6250.ramdisk \
     fstab.modem \
     init.hi6250.connectivity.rc \
     init.hi6250.hisi.rc \