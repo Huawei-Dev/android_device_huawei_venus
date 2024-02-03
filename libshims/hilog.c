b2699971ebb117e56e96784c444ff0b6f668efc2
Huawei-Dev <surdu_petru@yahoo.com>
2024-02-03 21:18:07 +0100

    venus:libshims: remove unneeded return on void function

--- libshims/hilog.c
+++ libshims/hilog.c
@@ -6,5 +6,4 @@
 
 void __hi_log_print(int param1, char* param2, char* param3, int param4) {
     ALOGV("%s: param1: %d, param2: %s, param3: %s, param4: %d", __func__, param1, param2, param3, param4);
-    return;
 }
