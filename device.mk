d587649c345cff9f3c1bad4df9898f08c6a8557d
Huawei_Dev <surdu_petru@yahoo.com>
2024-11-09 18:18:06 +0100

    venus: remove mploader service

--- device.mk
+++ device.mk
@@ -278,8 +278,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.radio@1.0.vendor \
-    librilutils \
-    mploader
+    librilutils
     
 # Shims
 PRODUCT_PACKAGES += \
