1732e8356b389d5edd5dfdf827e4a0c56fac3289
Huawei_Dev <surdu_petru@yahoo.com>
2024-11-09 19:24:20 +0100

    venus: drop audio_patch stuff

--- device.mk
+++ device.mk
@@ -41,9 +41,6 @@ PRODUCT_COPY_FILES += \
     
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/audio_patch:$(TARGET_COPY_OUT_VENDOR)/bin/audio_patch
-
-PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
     
 PRODUCT_COPY_FILES += \
