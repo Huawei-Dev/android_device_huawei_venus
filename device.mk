3764bd60f433ca627f4fa81801e34b438f233bb9
Huawei_Dev <surdu_petru@yahoo.com>
2024-11-10 16:14:04 +0100

    venus: use the new service light

--- device.mk
+++ device.mk
@@ -187,9 +187,7 @@ PRODUCT_PACKAGES += \
     
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.hi6250
+    android.hardware.light-service.huawei
     
 # Lineage hardware
 PRODUCT_PACKAGES += \
