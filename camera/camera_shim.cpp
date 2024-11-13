7fe001c8a50850572674fa6240a30ff80d0b5be2
Roger Ortiz <me@r0rt1z2.com>
2024-11-13 13:43:33 +0100

    venus: Add shim for libui Change-Id: Ibe10f0ebb9ae09e399e72ebb0d289a41e9993285

--- camera/camera_shim.cpp
+++ camera/camera_shim.cpp
@@ -17,6 +17,9 @@
 #include <string>
 #include <ui/GraphicBuffer.h>
 
+using android::PixelFormat;
+using android::status_t;
+
 extern "C" {
 
 void* _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(
@@ -44,6 +47,28 @@ void* _ZN7android13GraphicBufferC1EjjijjP13native_handleb(
         (keepOwnership ? android::GraphicBuffer::TAKE_HANDLE : android::GraphicBuffer::WRAP_HANDLE);
     return _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(thisptr, inHandle, inMethod, inWidth, inHeight,
         inFormat, static_cast<uint32_t>(1), static_cast<uint64_t>(inUsage), inStride);
+}
+
+#if defined(__LP64__)
+status_t _ZN7android19GraphicBufferMapper12importBufferEPK13native_handlejjjimjPS3_(
+        void* thisptr, buffer_handle_t rawHandle, uint32_t width, uint32_t height,
+        uint32_t layerCount, PixelFormat format, uint64_t usage, uint32_t stride,
+        buffer_handle_t* outHandle);
+#else
+status_t _ZN7android19GraphicBufferMapper12importBufferEPK13native_handlejjjiyjPS3_(
+        void* thisptr, buffer_handle_t rawHandle, uint32_t width, uint32_t height,
+        uint32_t layerCount, PixelFormat format, uint64_t usage, uint32_t stride,
+        buffer_handle_t* outHandle);
+#endif
+status_t _ZN7android19GraphicBufferMapper12importBufferEPK13native_handlePS3_(
+        void* thisptr, buffer_handle_t rawHandle, buffer_handle_t* outHandle) {
+#if defined(__LP64__)
+    return _ZN7android19GraphicBufferMapper12importBufferEPK13native_handlejjjimjPS3_(
+            thisptr, rawHandle, -1, -1, -1, android::PIXEL_FORMAT_NONE, -1, -1, outHandle);
+#else
+    return _ZN7android19GraphicBufferMapper12importBufferEPK13native_handlejjjiyjPS3_(
+            thisptr, rawHandle, -1, -1, -1, android::PIXEL_FORMAT_NONE, -1, -1, outHandle);
+#endif
 }
 
 } // extern "C"
