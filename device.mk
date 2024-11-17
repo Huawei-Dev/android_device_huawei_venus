3cc2d4d1e9c8f6e364ef3c382ae4ae61b15e83ca
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-11-17 12:59:04 +0100

    venus: Retrofit dynamic partitions * Mark standard block devices as super block devices (from   https://source.android.com/devices/tech/ota/dynamic_partitions/implement?hl=en#selinux-legacy)
    Change-Id: I2553aad99f5c458cf6ac6b4265dc5840127c13e9

--- device.mk
+++ device.mk
@@ -246,6 +246,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml
     
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr
