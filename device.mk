28f904c4b1f1719ec503d139cb03ce9b98211d73
Huawei_Dev <surdu_petru@yahoo.com>
2025-06-01 20:25:57 +0200

    venus: fixup build android.hardware.camera.provider@2.4-impl

--- device.mk
+++ device.mk
@@ -60,7 +60,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
-    android.hardware.camera.provider@2.4-impl:64
+    android.hardware.camera.provider@2.4-impl
 
 PRODUCT_PACKAGES += \
     libstdc++.vendor \
